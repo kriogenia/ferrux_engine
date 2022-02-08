fe3ea5ffc298d5dc88d9859553643bef25984e40
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2022-02-08 11:14:48 +0100

    Test vector multiplication

--- src/geometry/point3.rs
+++ src/geometry/point3.rs
@@ -1,5 +1,6 @@
-use crate::geometry::Matrix4;
+use crate::geometry::{Matrix4, MatrixBuilder};
 
+#[derive(Debug)]
 pub struct Point3 {
 	pub x: f32,
 	pub y: f32,
@@ -8,7 +9,7 @@ pub struct Point3 {
 
 impl Point3 {
 
-	fn multiply_matrix(&self, matrix: Matrix4) -> Option<Point3> {
+	fn multiply_matrix(&self, matrix: &Matrix4) -> Option<Point3> {
 		let x = self.x * matrix[0][0] + self.y * matrix[1][0] + self.z * matrix[2][0] + matrix[3][0];
 		let y = self.x * matrix[0][1] + self.y * matrix[1][1] + self.z * matrix[2][1] + matrix[3][1];
 		let z = self.x * matrix[0][2] + self.y * matrix[1][2] + self.z * matrix[2][2] + matrix[3][2];
@@ -25,4 +26,27 @@ impl Point3 {
 
 }
 
-// TODO test multiplication
+#[test]
+fn empty_matrix_multiplication() {
+	let point = Point3 { x: 1.0, y: 1.0, z: 1.0 };
+	let matrix = Matrix4::default();
+	assert!(point.multiply_matrix(&matrix).is_none());
+}
+
+#[test]
+fn vector_matrix_multiplication() {
+	let point = Point3 { x: 1.0, y: 1.0, z: 1.0 };
+	let matrix = MatrixBuilder::new()
+		.set_height(1)
+		.set_width(1)
+		.set_fov(90.0)
+		.set_view_limit(2.0)
+		.set_screen_position(1.0)
+		.build();
+
+	let result = point.multiply_matrix(&matrix).unwrap();
+	let expected = Point3 { x: 1.0, y: 1.0, z: 0.0 };
+	assert!((result.x - expected.x).abs() < 0.0001);
+	assert!((result.y - expected.y).abs() < 0.0001);
+	assert!((result.z - expected.z).abs() < 0.0001);
+}
